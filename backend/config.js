0af490600f5c06d6f56822a58e72a334878228ec
Jimmy Haboon <101476866+Jhaboon21@users.noreply.github.com>
2024-05-06 22:52:31 -0400

    Update config.js
    added url to online database

--- backend/config.js
+++ backend/config.js
@@ -7,7 +7,8 @@ require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
-const PORT = +process.env.PORT || 3001;
+// const PORT = +process.env.PORT || 3001;
+const PORT = +process.env.PORT || "postgres://liwigcws:iXEJsxjcldtOIR2UVM0obNrYmNcn-IBC@jelani.db.elephantsql.com/liwigcws"
 
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
