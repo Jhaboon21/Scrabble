501929264a71483e7d0339739f79ea6bb67ad4db
Jimmy Haboon <101476866+Jhaboon21@users.noreply.github.com>
2024-06-02 17:31:16 -0400

    Update useLocalStorage.js

--- frontend/src/hooks/useLocalStorage.js
+++ frontend/src/hooks/useLocalStorage.js
@@ -1,22 +1,28 @@
 import { useState, useEffect } from "react";
 
 /** custome hook for using local storage to keep state data updated */
-function useLocalStorage(key, firstValue = null) {
-    const initialValue = localStorage.getItem(key) || firstValue;
-  
-    const [item, setItem] = useState(initialValue);
-  
-    useEffect(function setKeyInLocalStorage() {
-        console.debug("hooks useLocalStorage useEffect", "item=", item);
-  
-        if (item === null) {
-            localStorage.removeItem(key);
-        } else {
-            localStorage.setItem(key, item);
-        }
-    }, [key, item]);
-  
-    return [item, setItem];
+function useLocalStorage(key, initialValue = null) {
+    const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = value => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [storedValue, setValue];
 }
   
 export default useLocalStorage;
